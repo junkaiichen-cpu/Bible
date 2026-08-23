b1658c911d9ebee94092299be168437d82339f86
junkaiichen-cpu <junkaiichen@gmail.com>
2026-08-24 00:00:22 +0200

    build: add Electron desktop shell

---
+++ electron/main.cjs
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 820,
+    minWidth: 980,
+    minHeight: 700,
+    backgroundColor: '#090807',
+    title: 'Bible Fighter',
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  });
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, '..', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
